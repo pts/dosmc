620687f77bebc52484c793a135150b0824f55c63
Peter Szabo <pts@fazekas.hu>
2020-06-27 09:08:30 +0200

    added examples/macros.c

---
+++ examples/macros.c
@@ -0,0 +1,12 @@
+/* ./dosmc -pl examples/macros.c */
+/* http://www.x-hacker.org/ng/wcppug/ng91a37.html */
+"__WATCOMC__" __WATCOMC__
+"__WATCOM_CPLUSPLUS__" __WATCOM_CPLUSPLUS__
+"__DOSMC__" __DOSMC__
+"__DOSMC_COM__" __DOSMC_COM__
+"__DOS__" __DOS__
+"__X86__" __X86__
+"__I86__" __I86__
+"__386__" __386__  /* Not defined. */
+"_M_IX86" _M_IX86  /* Flag -6 --> 600 */
+"__SMALL__" __SMALL__  /* For both .com and .exe */
