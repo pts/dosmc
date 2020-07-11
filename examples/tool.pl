865f3a21247b4d8bd110505b699fc0982adc6fd9
Peter Szabo <pts@fazekas.hu>
2020-07-11 16:46:42 +0200

    added Perl script execution mode: `dosmc myscript.pl ...'

---
+++ examples/tool.pl
@@ -0,0 +1,10 @@
+# Run this Perl script from any directory as: .../dosmc examples/tool
+
+my $a = join(", ", @ARGV);
+print "0=$0\n";
+print "ARGV=($a)\n";
+print "PATH=($ENV{PATH})\n";
+print "INC=@INC\n";
+# Existing Perl function in dosmc can be used.
+die "missing fix_path\n" if !defined(*main::fix_path);
+print "tool OK.\n";
