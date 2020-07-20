0296bc71f8991f3b3f9a2f05a89e7ad99fa88e19
Peter Szabo <pts@fazekas.hu>
2020-07-20 20:41:54 +0200

    added @echo off to dosmc.cmd, without it Windows 10 did echo some of the commands executed

--- dosmc.cmd
+++ dosmc.cmd
@@ -1,13 +1,14 @@
-@set m=%~dpn0.cmd
-@for %%f in ("%~n0.cmd") do if not exist "%~dpn0.cmd" set m=%%~dpn$PATH:f.cmd
-@if "%m%"==".cmd" echo fatal: program not found: %~n0.cmd 1>&2 & exit /b 1
-@for %%f in ("%m%") do set c=%%~dpfdosmc.dir\dosmc.pl
-@if not exist "%c%" echo fatal: script not found: %c% 1>&2 & exit /b 1
-@for %%f in ("%c%") do set p=%%~dpfperl.exe
-@if not exist "%p%" set p=perl
-@for %%f in ("%c%") do set r=%%~dpfpreamblew.pm
-@if not exist "%r%" echo fatal: preamble not found: %r% 1>&2 && exit /b 1
-@goto :last
+@echo off
+set m=%~dpn0.cmd
+for %%f in ("%~n0.cmd") do if not exist "%~dpn0.cmd" set m=%%~dpn$PATH:f.cmd
+if "%m%"==".cmd" echo fatal: program not found: %~n0.cmd 1>&2 & exit /b 1
+for %%f in ("%m%") do set c=%%~dpfdosmc.dir\dosmc.pl
+if not exist "%c%" echo fatal: script not found: %c% 1>&2 & exit /b 1
+for %%f in ("%c%") do set p=%%~dpfperl.exe
+if not exist "%p%" set p=perl
+for %%f in ("%c%") do set r=%%~dpfpreamblew.pm
+if not exist "%r%" echo fatal: preamble not found: %r% 1>&2 && exit /b 1
+goto :last
 #!perl -w
 BEGIN { $^W = 1 }
 @INC = ();  # Perl modules not installed, don't try to load them.
@@ -29,4 +30,4 @@ die "$0: fatal: error loading script: $script_fn: $!\n" if
     !defined($result) and $!;
 __END__
 :last
-@"%p%" -x "%m%" "%0" "%r%" "%c%" %*
+"%p%" -x "%m%" "%0" "%r%" "%c%" %*
