1f2c3761d0797ecef0bd48f7f51c65190601a941
Peter Szabo <pts@fazekas.hu>
2022-05-27 22:48:49 +0200

    added read(...) to libc

--- dosmc.dir/dosmc.h
+++ dosmc.dir/dosmc.h
@@ -250,6 +250,7 @@ modify [ ah ];
 int remove(const char *pathname);
 int unlink(const char *pathname);  /* Same as remove(). */
 
+ssize_t read(int fd, void *buf, size_t count);
 ssize_t write(int fd, const void *buf, size_t count);
 
 #endif  /* _DOSMC_H_ */
