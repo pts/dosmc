1358a86f5382afa5f1dff9a5fccfe2fa4d1dc4d0
Peter Szabo <pts@fazekas.hu>
2022-05-28 02:54:54 +0200

    added processing of segment-base relocation with mov dx, ...

---
+++ examples/helloid.asm
@@ -0,0 +1,14 @@
+ideal  ; Optional for LZASM. Makes this file work with TASM as well. Also dosmc will autodetect it.
+model small
+
+dataseg  ; Set segment alignment to byte (now it's word).
+lpText:	db "Hello, world!$"
+
+codeseg
+	startupcode  ; Works in TASM 2.0, doesn't work in TASM 1.01.
+	lea dx,[lpText]  ; TASM 3.0 converts this to a mov, WASM doesn't.
+	mov dx,offset lpText
+	mov ah,9
+	int 21h
+	exitcode 0  ; Works in TASM 3.0, it doesn't work in TASM 2.51.
+end  ; Also optional for LZASM. TASM 5.0 needs it.
